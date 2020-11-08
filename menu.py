c17dbac81d0f0ea0e563a9ec0f4eb403f552e183
manan-bedi2908 <mananbedilps@gmail.com>
2020-11-09 00:59:53 +0530

    added

--- menu.py
+++ menu.py
@@ -28,10 +28,101 @@ def web_server_config():
 	print("Web Server Started")
 	os.system("cd /var/www/html")
 
+def create_new_user():
+	print("Enter the UserName: ",end=' ')
+	userName = input()
+	os.system("useradd {}".format(userName))
+	print("Enter the password", end=' ')
+	password = input()
+	os.system("passwd {}".format(userName))
+
 def create_file():
 	print("Enter The File Name: ",end=' ')
 	file_name = input()
 	os.system("touch {}".format(file_name))
+
+def check_software():
+	print("Enter the Software Name: ",end=' ')
+	software_name = input() 
+	os.system("rpm -q {}".format(software_name))
+
+def install_software():
+	print("Enter the Software Name: ", end=' ')
+	software_name = input()
+	os.system("yum install {}".format(software_name))
+
+def launch_container():
+	print("Enter the Operating System Name (Operating System:version): ", end='')
+	os_name = input()
+	os.system("docker pull {}".format(os_name))
+	print("Pulled the container image successfully from Docker Container")			
+	os.system("docker run -it {}".format(os_name))
+	print("Entered into the Docker Container")
+
+def create_instance():
+	image_id = input("Enter the Image ID: ")
+	instance_type = input("Enter Instance Type: ")
+	count = int(input("Enter the Number of Instances you want to launch: "))
+	subnet_id = input("Enter the Subnet ID: ")
+	sg_group_id = input("Enter the Security Group ID: ")
+	key_name = input("Enter the Key Name: ")
+	os.system(f"aws ec2 run-instances --image-id {image_id} --instance-type {instance_type} --count {count} --subnet-id {subnet_id} --security-group-ids {sg_group_id} --key-name {key_name}")
+
+def attach_EBS_Volume():
+	zone = input("Enter the Availability Zone (Should be same with the instance you want to attach): ")
+	size = int(input("Enter the size of the EBS Volume (In GB): "))
+	os.system(f"aws ec2  create-volume --availability-zone {zone}   --size {size}")
+	instance_id = input("Enter the Instance ID you wish to attach EBS Volume with: ")
+	volume_id = input("Enter the Volume ID: ")
+	os.system(f"aws ec2 attach-volume  --device   /dev/sdg   --instance-id  {instance_id} --volume-id {volume_id}")
+	print(f"Attached EBS Volume with the Instance of {size} GB")
+
+def create_partition():
+	print("Create, Format and Mount a Partition in a Virtual Hard Disk")
+	os.system("tput setaf 9")
+	print("Make sure you have attached a new hard disk to the OS already")
+	os.system("tput setaf 11")
+	print("Displaying all the Hard Disks Available")
+	os.system("fdisk -l")
+	os.system("tput setaf 14")
+	print("Now, create a partition in the Desired Hard Disk")
+	hard_disk = input("Enter the Hard Disk: ")
+	os.system("fdisk {}".format(hard_disk))
+	os.system("udevadm settle")	
+	os.system("tput setaf 7")
+	print("Driver Installed")
+	partition = hard_disk + "1"
+	os.system(f"mkfs.ext4 {partition}")
+	os.system("tput setaf 13")
+	print("Successfully formatted the Partition!!")
+	folder = input("Enter the directory location to be mounted with the new partition: ")
+	os.system(f"mount {partition} {folder}")
+	os.system("tput setaf 7")
+	os.system("Displaying the information of the disks !!")
+	os.system("df -h")
+
+def create_s3():
+	print("Create a S3 Bucket")	
+	bucket_name = input("Enter the name of the Bucket: ")
+	region = input("Enter the name of the region: ")	
+	os.system(f"aws s3api create-bucket --bucket {bucket_name} --region {region}")
+
+def upload_to_S3():
+	bucket_name = input("Enter the name of the Bucket: ")
+	file_name = input("Enter the location of the object to be uploaded to S3 Bucket: ")
+	name = input("Enter the name of the object in the S3 Bucket: ")
+	os.system(f"aws s3 cp {file_name} s3://{bucket_name}/{name}")
+	os.system("tput setaf 11")
+	public = input("Do you want to make the file in this Bucket public (Y/N): ")
+	if public == 'Y':
+		os.system("aws s3api put-object-acl --bucket {bucket_name} --key pic1.jpg --acl public-read")
+	print("Successfully uploaded the object to the S3 Bucket!!")
+
+def create_cloudfront():
+	print("Create a CloudFront Distribution using S3 as Origin")
+	bucket_name = input("Enter the name of the Bucket: ")
+	file_name = input("Enter the file name in the Bucket: ")
+	os.system(f"aws cloudfront create-distribution --origin-domain-name {bucket_name}.s3.amazonaws.com --default-root-object {file_name}")
 
 while True:
 	print("""
@@ -66,92 +157,31 @@ Press 0: Exit
 		elif int(ch) == 3:
 			web_server_config()
 		elif int(ch) == 4:
-			print("Enter the UserName: ",end=' ')
-			userName = input()
-			os.system("useradd {}".format(userName))
-			print("Enter the password", end=' ')
-			password = input()
-			os.system("passwd {}".format(userName))
+			create_new_user()
 		elif int(ch) == 5:
 			create_file()
 		elif int(ch) == 6:
 			os.system("fdisk -l")
 		elif int(ch) == 7:
-			print("Enter the Software Name: ",end=' ')
-			software_name = input() 
-			os.system("rpm -q {}".format(software_name))
+			check_software()
 		elif int(ch) == 8:
-			print("Enter the Software Name: ", end=' ')
-			software_name = input()
-			os.system("yum install {}".format(software_name))
+			install_software()
 		elif int(ch) == 9:
-			print("Enter the Operating System Name (Operating System:version): ", end='')
-			os_name = input()
-			os.system("docker pull {}".format(os_name))
-			print("Pulled the container image successfully from Docker Container")			
-			os.system("docker run -it {}".format(os_name))
-			print("Entered into the Docker Container")
+			launch_container()
 		elif int(ch) == 11:
 			os.system("aws ec2 describe-instances")
 		elif int(ch) == 12:
-			image_id = input("Enter the Image ID: ")
-			instance_type = input("Enter Instance Type: ")
-			count = int(input("Enter the Number of Instances you want to launch: "))
-			subnet_id = input("Enter the Subnet ID: ")
-			sg_group_id = input("Enter the Security Group ID: ")
-			key_name = input("Enter the Key Name: ")
-			os.system(f"aws ec2 run-instances --image-id {image_id} --instance-type {instance_type} --count {count} --subnet-id {subnet_id} --security-group-ids {sg_group_id} --key-name {key_name}")
+			create_instance()
 		elif int(ch) == 13:
-			zone = input("Enter the Availability Zone (Should be same with the instance you want to attach): ")
-			size = int(input("Enter the size of the EBS Volume (In GB): "))
-			os.system(f"aws ec2  create-volume --availability-zone {zone}   --size {size}")
-			instance_id = input("Enter the Instance ID you wish to attach EBS Volume with: ")
-			volume_id = input("Enter the Volume ID: ")
-			os.system(f"aws ec2 attach-volume  --device   /dev/sdg   --instance-id  {instance_id} --volume-id {volume_id}")
-			print(f"Attached EBS Volume with the Instance of {size} GB")
+			attach_EBS_Volume()
 		elif int(ch) == 14: 
-			print("Create, Format and Mount a Partition in a Virtual Hard Disk")
-			os.system("tput setaf 9")
-			print("Make sure you have attached a new hard disk to the OS already")
-			os.system("tput setaf 11")
-			print("Displaying all the Hard Disks Available")
-			os.system("fdisk -l")
-			os.system("tput setaf 14")
-			print("Now, create a partition in the Desired Hard Disk")
-			hard_disk = input("Enter the Hard Disk: ")
-			os.system("fdisk {}".format(hard_disk))
-			os.system("udevadm settle")
-			os.system("tput setaf 7")
-			print("Driver Installed")
-			partition = hard_disk + "1"
-			os.system(f"mkfs.ext4 {partition}")
-			os.system("tput setaf 13")
-			print("Successfully formatted the Partition!!")
-			folder = input("Enter the directory location to be mounted with the new partition: ")
-			os.system(f"mount {partition} {folder}")
-			os.system("tput setaf 7")
-			os.system("Displaying the information of the disks !!")
-			os.system("df -h")
+			create_partition()
 		elif int(ch) == 15:
-			print("Create a S3 Bucket")
-			bucket_name = input("Enter the name of the Bucket: ")
-			region = input("Enter the name of the region: ")	
-			os.system(f"aws s3api create-bucket --bucket {bucket_name} --region {region}")
+			create_S3()
 		elif int(ch) == 16:
-			bucket_name = input("Enter the name of the Bucket: ")
-			file_name = input("Enter the location of the object to be uploaded to S3 Bucket: ")
-			name = input("Enter the name of the object in the S3 Bucket: ")
-			os.system(f"aws s3 cp {file_name} s3://{bucket_name}/{name}")
-			os.system("tput setaf 11")
-			public = input("Do you want to make the file in this Bucket public (Y/N): ")
-			if public == 'Y':
-				os.system("aws s3api put-object-acl --bucket {bucket_name} --key pic1.jpg --acl public-read")
-			print("Successfully uploaded the object to the S3 Bucket!!")
+			upload_to_S3()
 		elif int(ch) == 17:
-			print("Create a CloudFront Distribution using S3 as Origin")
-			bucket_name = input("Enter the name of the Bucket: ")
-			file_name = input("Enter the file name in the Bucket: ")
-			os.system(f"aws cloudfront create-distribution --origin-domain-name {bucket_name}.s3.amazonaws.com --default-root-object {file_name}")
+			create_cloudfront()
 		elif int(ch) == 0:
 			exit()
 		else: 	
